39a518d13c2a5c72bc5c1ec67d18616d30be93cd
anaur <124123553+a-anaur@users.noreply.github.com>
2024-02-02 19:09:57 +0200

    Add files via upload

---
+++ assets/main.js
@@ -0,0 +1,5 @@
+let navIconsContainer = document.getElementById('nav-icons');
+let navIcons = document.querySelector('.navbar-items');
+
+navIconsContainer.appendChild(navIcons);
+navIcons.classList.remove('d-none');
